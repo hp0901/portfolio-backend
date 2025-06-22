50f1fb08905400bccdf0f0e9e554ab55f73d3d6f
hp0901 <hp98570018@gmail.com>
2025-06-22 09:19:46 +0530

    Update frontend and backend changes

--- src/Header/ContactPage.js
+++ src/Header/ContactPage.js
@@ -52,7 +52,7 @@ const ContactPage = () => {
     }
 
     try {
-      const response = await fetch("https://portfolio-un3dk99oe-harsh-patels-projects-0210bdd8.vercel.app", {
+      const response = await fetch("portfolio-km4lbiasb-harsh-patels-projects-0210bdd8.vercel.app", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
